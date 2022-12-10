8324a54fa98d670cedc80d9a6fdf61b7b087f4b6
JM-Flores <juanmiguel.flores.cics@ust.edu.ph>
2022-12-10 22:28:24 +0800

    limit1300

---
+++ LimitSamples1300.py
@@ -0,0 +1,25 @@
+# Remove files to limit each classification to 1300 samples
+
+import os
+import shutil
+
+
+def moveFiles():
+    source_folder = r"D:\python\Final Project\age_types\25-30"
+    destination_folder = r"D:\python\Final Project\age_types\25-30-final"
+
+    file_names = os.listdir(source_folder)
+
+    for file_name in file_names[1300:]:
+        shutil.move(os.path.join(source_folder, file_name), destination_folder)
+
+
+def main():
+    moveFiles()
+
+
+if name == "main":
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
